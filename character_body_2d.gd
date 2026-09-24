6fb9b26a19a874ee24561bc8a08f53dd6260c190
jonavinbrock <62173@student.baisd.net>
2026-09-24 11:54:54 -0400

    good movement techniques

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -2,9 +2,9 @@ extends CharacterBody2D
 
 @export var player: CharacterBody2D
 
-const SPEED = 300.0
+const SPEED = 5.0
 const JUMP_VELOCITY = -400.0
-
+const grid_size = 48
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -18,9 +18,6 @@ func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	position.x += direction * grid_size
+	position.x = round(position.x)
 	move_and_slide()
